80368a8af55803284dbf419a117bbd8f712a31ed
Hadar Gamliel <hgamliel@berkeley.edu>
2023-04-10 13:29:38 -0700

    tuning parameters with updated testing impulse force

--- cassie_simulator/cassie/other_functions/ExternalForce.m
+++ cassie_simulator/cassie/other_functions/ExternalForce.m
@@ -2,8 +2,12 @@ function f_ext = ExternalForce(t, q,model)
 % F_pert 6x1 - roll, pitch, yaw, x,y,z
 F_pert =       7.5*[1 -1 1            -1 1 -1]';
 F_pert =       0*[1 -1 1            -1 1 -1]';
-F_pert = [0 0 0 10 0 0]';
 
+if t<.1
+    F_pert = [0 0 0 model.M*9.81*.5 0 0]';
+else
+    F_pert = zeros(6,1);
+end
 
 % apply perturbation force on torso
 f_ext = cell(1,model.NB);
